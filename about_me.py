ac4e972a94c92ad925c9bf7f955efc4fd55bde5b
Anthony Vuong <38023961+AntonWontonn@users.noreply.github.com>
2022-10-13 17:38:03 -0700

    aboutMe assignment

---
+++ about_me.py
@@ -0,0 +1,14 @@
+fullname = "Anthony Vuong"
+nickname = "Ant"
+age = 16
+usedPython = True
+favoriteHobbies = {"playing games", "watching shows", "finding new music", "taking walks"}
+favoriteThings = {
+  "Favorite Views" : "Sunsets and Foggy Mornings",
+  "Games" : "Valorant",
+  "Hobby" : "Listening to music",
+  "Anime" : "Re:Zero",
+  "Misc" : "Rollercoasters"
+}
+
+all_vars = dict(vars())
